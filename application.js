ada653f4151b6f45532be7e7437353e799b3c623
andres9366ha <andres9366ha@gmail.com>
2016-08-05 14:03:48 -0600

    commit 05 08 2016

--- application.js
+++ application.js
@@ -1,52 +1,61 @@
-window.onload = function Probar(){
-    var x = new Array();
-    var y = localStorage.getItem('rides');
-    x = JSON.parse(y);
-    var table = document.createElement("TABLE");
-    table.border = "1";
-
-    var ths = ["user", "start", "end", "Actions"];
-    var columnCount = ths.length;
-
-
-    //Add Header Row
-    var row = table.insertRow(-1);
-    for (var i = 0; i < columnCount; i++) {
-      var headerCell = document.createElement("TH");
-      headerCell.innerHTML = ths[i];
-      row.appendChild(headerCell);
-    }
-
-    //Add the data rows.
-    for (var i = 0; i < x.length; i++) {
-      row = table.insertRow(-1);
-      for (var j = 0; j < ths.length; j++) {
-        var cell = row.insertCell(-1);
-        //cell.innerHTML = x[i].number;
-        switch (j) {
-          case 0:
-            cell.innerHTML = x[i].user;
-            break;
-          case 1:
-            cell.innerHTML = x[i].start;
-            break;
-          case 2:
-            cell.innerHTML = x[i].end;
-            break;
-          case 3:
-          cell.innerHTML = '<a href="login.html">View</a>';
-          break;
-          default: cell.innerHTML = 0;
+function checkInputs(){
+  for (var i = 0; i < alignedForm.length; i++) {
+    if((alignedForm[i].type == 'text') || (alignedForm[i].type == 'password')){
+      var checkTest = alignedForm[i].value.replace(/\s/gi,'');
+      if(checkTest == null || checkTest.length == 0) {
+        alert("Campo sin rellenar -> " + alignedForm[i].id);
+        document.alignedForm.alignedForm[i].focus();
+        return false;
         }
       }
     }
+    return true;
+};
 
-    var dvTable = document.getElementById("dvTable");
-    dvTable.innerHTML = "";
-    dvTable.appendChild(table);
-
-/*
-    for (var i = 0; i < x.length; i++) {
-      alert('Nombre: '+ x[i].name + ', apellido: ' + x[i].lastname + ', número telefónico: ' + x[i].number);
-    }*/
+document.getElementById('addUser').addEventListener("click", function generalValidated(){
+  if(checkInputs()){
+    if(checkPasswords()){
+        alert("Ha sido true");
+    }else {
+      alert("Error en registro");
+      document.alignedForm.pass.required;
+    }
   }
+});
+
+
+function checkPasswords(){
+  var pass1 = document.getElementById('pass').value;
+  var pass2 = document.getElementById('repeatpass').value;
+  if(pass1 == pass2){
+    return true;
+  }else {
+    alert("Error passwords");
+    return false;
+  }
+}
+
+function checkUser(){
+  //Si sale bien acá llamo a saveUser()
+}
+
+function createUser(){
+  var person = new Object();
+  person.name = document.getElementById('name').value;
+  person.lastname = document.getElementById('lastname').value;
+  person.phone = document.getElementById('phone').value;
+  person.username = document.getElementById('username').value;
+  person.password = document.getElementById('pass').value;
+  person.repeatpass = document.getElementById('repeatpass').value;
+  saveUser(person);
+}
+
+function saveUser(person){
+  var myArray = [];
+  myArray = JSON.parse(localStorage.getItem('rideUsers'));
+  myArray = myArray || [];     /*check if it's not null and create one if needed*/
+  myArray.push(person);
+  localStorage.setItem('rideUsers', JSON.stringify(myArray));
+  alert("User Added");
+  location.reload(true);
+}
